d796a837da0f61a1b2c8ce0c5ee77f80938f76e2
Christof Stromberger <stromberger@student.tugraz.at>
2012-09-24 11:59:35 +0200

    Added further unit tests

--- IAIK Encryption AppTests/CryptoTests.m
+++ IAIK Encryption AppTests/CryptoTests.m
@@ -9,6 +9,7 @@
 #import "CryptoTests.h"
 #import "Crypto.h"
 #import "X509CertificateUtil.h"
+#import "Base64.h"
 
 //sample values
 #define SAMPLE_NAME @"Christof Stromberger"
@@ -17,6 +18,8 @@
 #define SAMPLE_CITY @"Graz"
 #define SAMPLE_ORGANIZATION @"Graz University of Technology"
 #define SAMPLE_ORG_UNIT @"IAIK"
+#define SAMPLE_CONTAINER @"Hello World! This is an encrypted container!"
+#define SAMPLE_CONTAINER_ENCODED @"SGVsbG8gV29ybGQhIFRoaXMgaXMgYW4gZW5jcnlwdGVkIGNvbnRhaW5lciE="
 
 
 @interface CryptoTests()
@@ -44,9 +47,13 @@
 - (NSData*)newPrivateKey {
     return [self.crypto createRSAKey];
 }
-- (NSData*)newCertificate {
+
+- (NSData*)newCertificateWithPrivateKey:(NSData*)privateKey {
     
-    return [self.crypto createX509CertificateWithPrivateKey:[self newPrivateKey] withName:SAMPLE_NAME emailAddress:SAMPLE_EMAIL country:SAMPLE_COUNTRY city:SAMPLE_CITY organization:SAMPLE_ORGANIZATION organizationUnit:SAMPLE_ORG_UNIT];
+    return [self.crypto createX509CertificateWithPrivateKey:privateKey withName:SAMPLE_NAME emailAddress:SAMPLE_EMAIL country:SAMPLE_COUNTRY city:SAMPLE_CITY organization:SAMPLE_ORGANIZATION organizationUnit:SAMPLE_ORG_UNIT];}
+
+- (NSData*)newCertificate {
+    return [self newCertificateWithPrivateKey:[self newPrivateKey]];
 }
 
 //private key creation test
@@ -56,6 +63,7 @@
 }
 
 
+//certificate creation test case
 - (void)test002_createNewCertificate {
     NSData *certificate = [self newCertificate];
     STAssertNotNil(certificate, @"checking certificate");
@@ -65,9 +73,53 @@
     STAssertEqualObjects([X509CertificateUtil getCity:certificate], SAMPLE_CITY, @"checking city");
     STAssertEqualObjects([X509CertificateUtil getOrganization:certificate], SAMPLE_ORGANIZATION, @"checking organization");
     STAssertEqualObjects([X509CertificateUtil getOrganizationUnit:certificate], SAMPLE_ORG_UNIT, @"checking organization unit");
+}
+
+//encrypting data using a new certificate
+- (void)test003_encryptData {
+    NSData *privateKey = [self newPrivateKey];
+    NSData *certificate = [self newCertificateWithPrivateKey:privateKey];
+    
+    STAssertNotNil(privateKey, @"checking private key");
+    STAssertNotNil(certificate, @"checking certificate");
+    
+    NSString *testString = SAMPLE_CONTAINER;
+    STAssertNotNil(testString, @"checking test string");
+    NSData *testData = [testString dataUsingEncoding:NSUTF8StringEncoding];
+    STAssertNotNil(testData, @"checking test data");
     
     
+    NSData *encryptedContainer = [self.crypto encryptBinaryFile:testData withCertificate:certificate];
+    STAssertNotNil(encryptedContainer, @"checking container");
     
+    NSData *decryptedContainer = [self.crypto decryptBinaryFile:encryptedContainer withUserCertificate:certificate privateKey:privateKey];
+    STAssertNotNil(decryptedContainer, @"checking decryption");
+    
+    NSString *decryptedTestString = [NSString stringWithUTF8String:[decryptedContainer bytes]];
+    STAssertNotNil(decryptedTestString, @"checking decrypted test string");
+    
+    STAssertEqualObjects(decryptedTestString, SAMPLE_CONTAINER, @"checking contents of container");
+}
+
+
+//base64 encode and decode tests
+- (void)test004_testBase64Encode {
+    NSString *testString = SAMPLE_CONTAINER;
+    STAssertNotNil(testString, @"checking test string");
+    STAssertEqualObjects([Base64 encodeBase64WithString:testString], SAMPLE_CONTAINER_ENCODED, @"checking base64 encoding");
+    
+    NSData *testBytes = [testString dataUsingEncoding:NSUTF8StringEncoding];
+    STAssertNotNil(testBytes, @"checking test bytes");
+    STAssertEqualObjects([Base64 encodeBase64WithData:testBytes], SAMPLE_CONTAINER_ENCODED, @"checking base64 encoding");
+}
+
+- (void)test005_testBase64Decoce {
+    NSString *testString = SAMPLE_CONTAINER_ENCODED;
+    NSString *originalTestString = SAMPLE_CONTAINER;
+    NSData *testBytes = [originalTestString dataUsingEncoding:NSUTF8StringEncoding];
+    STAssertNotNil(testString, @"checking test string");
+    STAssertNotNil(testBytes, @"checking test bytes");
+    STAssertEqualObjects([Base64 decodeBase64WithString:testString], testBytes, @"checking base64 decoding");
 }
 
 @end
